dd82e8a262a8c2ed7017211395491e464fbc40a0
Alex Marcon <amarcon@shovelhead>
2012-06-07 10:45:07 -0400

    Creating Project Workspace

---
+++ src/com/macadamian/temperature/TemperatureActivity.java
@@ -0,0 +1,13 @@
+package com.macadamian.temperature;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TemperatureActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
